1e44b8c6095459ad34fb8397ecf5114a19c86202
iukim21c <iukim21c@smartlo.net>
2024-07-08 13:56:24 +0900

    1. 최초 커밋

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_state_sample
+
+flutter에 state 라이브러리를 사용해본다.
+
+## Getting Started
+
+
---
+++ android/app/src/main/kotlin/com/grepiu/ex/flutter_state_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.grepiu.ex.flutter_state_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
